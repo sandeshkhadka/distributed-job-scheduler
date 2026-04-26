267826696dbd34db70b7f713b173ebc3faf9107c
sijanpanta <cjan1102@gmail.com>
2026-04-26 14:47:28 +0545

    feat: implment get_job_by_id

--- scheduler/scheduler_db.cpp
+++ scheduler/scheduler_db.cpp
@@ -1,5 +1,15 @@
 #include "scheduler_db.h"
 #include "database.hpp"
+#include <iostream>
+#include <vector>
+
+int get_job_cb(void* container, int argc, char** argv, char** col_name) {
+    std::vector<Job>* job = static_cast<std::vector<Job>*>(container);
+    for (int i = 0; i < argc; i++) {
+        job->emplace_back(Job{std::stoi(argv[0]), argv[1]});
+    }
+    return 0;
+}
 
 SchedulerDatabase::SchedulerDatabase() {
     Database::init("scheduler.db");
@@ -12,4 +22,14 @@ SchedulerDatabase::SchedulerDatabase() {
 void SchedulerDatabase::insert_job(const std::string& payload) {
     std::string query = "INSERT INTO jobs (payload) VALUES (\"" + payload + "\")";
     Database::instance().execute(query);
+}
+
+Job SchedulerDatabase::get_jobs_by_id(int id) {
+    std::string query = "SELECT * FROM jobs WHERE id = " + std::to_string(id);
+    std::vector<Job> jobs;
+    Database::instance().execute(query, get_job_cb, &jobs);
+    if (jobs.empty()) {
+        return Job{0, ""};
+    }
+    return jobs[0];
 }
